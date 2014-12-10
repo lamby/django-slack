91fade82a437db9b5c53a05723c833ae870cbf59
Chris Lamb <chris@chris-lamb.co.uk>
2014-12-10 20:31:47 +0000

    Add requests backend.
    Signed-off-by: Chris Lamb <chris@chris-lamb.co.uk>

--- django_slack/backends.py
+++ django_slack/backends.py
@@ -14,6 +14,23 @@ class UrllibBackend(Backend):
             if not fail_silently:
                 raise
 
+class RequestsBackend(Backend):
+    def __init__(self):
+        # Lazily import requests to avoid dependency
+        import requests
+
+        self.session = requests.Session()
+
+    def send(self, url, data, fail_silently):
+        try:
+            result = self.session.post(url, data=data, verify=False).json()
+
+            if not result['ok']:
+                raise ValueError(result['error'])
+        except Exception:
+            if not fail_silently:
+                raise
+
 class ConsoleBackend(Backend):
     def send(self, url, data, fail_silently):
         print "I: Slack message:"
