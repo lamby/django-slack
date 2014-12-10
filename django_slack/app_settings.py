7df740ea5793b262dad84a8109a7473c8ea1154e
Chris Lamb <chris@chris-lamb.co.uk>
2014-12-10 20:28:57 +0000

    Reflow and reorganise.
    Signed-off-by: Chris Lamb <chris@chris-lamb.co.uk>

--- django_slack/app_settings.py
+++ django_slack/app_settings.py
@@ -3,15 +3,12 @@ from django.conf import settings
 def setting(suffix, default):
     return getattr(settings, 'SLACK_%s' % suffix, default)
 
-BACKEND = setting(
-    'BACKEND',
-    'django_slack.backends.%s' % ('DisabledBackend' if settings.DEBUG else 'UrllibBackend'),
-)
-
 TOKEN = setting('TOKEN', None)
 CHANNEL = setting('CHANNEL', '#general')
 USERNAME = setting('USERNAME', 'bot')
 ICON_URL = setting('ICON_URL', None)
 ICON_EMOJI = setting('ICON_EMOJI', None)
 
+BACKEND = setting('BACKEND', 'django_slack.backends.%s' %
+    ('DisabledBackend' if settings.DEBUG else 'UrllibBackend'))
 FAIL_SILENTLY = setting('FAIL_SILENTLY', False)
