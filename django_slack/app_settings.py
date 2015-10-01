40c006653a81dd7df92a14d414bf83a61a53d58e
Chris Lamb <chris@chris-lamb.co.uk>
2015-10-01 21:18:28 +0200

    Support custom endpoint URLs (#9)

--- django_slack/app_settings.py
+++ django_slack/app_settings.py
@@ -1,5 +1,7 @@
 from django.conf import settings
 
+DEFAULT_ENDPOINT_URL = 'https://slack.com/api/chat.postMessage'
+
 def setting(suffix, default):
     return getattr(settings, 'SLACK_%s' % suffix, default)
 
@@ -8,6 +10,7 @@ CHANNEL = setting('CHANNEL', '#general')
 USERNAME = setting('USERNAME', 'bot')
 ICON_URL = setting('ICON_URL', None)
 ICON_EMOJI = setting('ICON_EMOJI', None)
+ENDPOINT_URL = setting('ENDPOINT_URL', DEFAULT_ENDPOINT_URL)
 
 BACKEND = setting('BACKEND', 'django_slack.backends.%s' %
     ('DisabledBackend' if settings.DEBUG else 'Urllib2Backend'))
