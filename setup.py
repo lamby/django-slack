006d74d68db3934f283dc672877cada1680a195d
Chris Lamb <chris@chris-lamb.co.uk>
2015-10-15 12:23:43 +0100

    use " for human text.

--- setup.py
+++ setup.py
@@ -10,8 +10,8 @@ setup(
     description="Provides easy-to-use integration between Django projects and "
         "the Slack group chat and IM tool.",
 
-    author='Chris Lamb',
-    author_email='chris@chris-lamb.co.uk',
+    author="Chris Lamb",
+    author_email="chris@chris-lamb.co.uk",
     license="BSD",
 
     packages=find_packages(),
