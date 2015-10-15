c1037b9e2aeafa260060618a1d3362df8ac48092
Chris Lamb <chris@chris-lamb.co.uk>
2015-10-15 12:23:16 +0100

    Wrap description line.

--- setup.py
+++ setup.py
@@ -7,7 +7,8 @@ setup(
 
     url="https://chris-lamb.co.uk/projects/django-slack",
     version='4.0.1',
-    description="Provides easy-to-use integration between Django projects and the Slack group chat and IM tool.",
+    description="Provides easy-to-use integration between Django projects and "
+        "the Slack group chat and IM tool.",
 
     author='Chris Lamb',
     author_email='chris@chris-lamb.co.uk',
