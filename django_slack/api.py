lamby/django-slack

commit 49ff3c4fe261a8c6a139177c75900cf848e52300

Rework fields into a definition-based system.
Signed-off-by: Chris Lamb <chris@chris-lamb.co.uk>

django_slack/api.py
@@ -10,64 +10,74 @@ from .utils import from_dotted_path
 backend = from_dotted_path(app_settings.BACKEND)()
 
 def slack_message(template, context=None, attachments=None, fail_silently=app_settings.FAIL_SILENTLY):
-    context = Context(context or {})
+    data = {}
 
+    context = Context(context or {})
     context['settings'] = settings
 
-    def render(component):
-        component_template = 'django_slack/%s' % component
+    for k, v in {
+        'text': {
+            'default': '',
+            'required': True,
+        },
+        'token': {
+            'default': app_settings.TOKEN,
+            'required': True,
+        },
+        'channel': {
+            'default': app_settings.CHANNEL,
+            'required': False, # Checked later
+        },
+        'icon_url': {
+            'default': app_settings.ICON_URL,
+            'required': False,
+        },
+        'icon_emoji': {
+            'default': app_settings.ICON_EMOJI,
+            'required': False,
+        },
+        'username': {
+            'default': app_settings.USERNAME,
+            'required': False,
+        },
+        'attachments': {
+            'default': attachments,
+            'render': False,
+            'required': False,
+        },
+        'endpoint_url': {
+            'default': app_settings.ENDPOINT_URL,
+            'render': False,
+            'required': False,
+        },
+    }.items():
+        # First, set from default if we have one
+        if v['default']:
+            data[k] = v['default']
 
-        return render_to_string(template, {
-            'django_slack': component_template,
-        }, context).strip().encode('utf8', 'ignore')
+        # Render template if necessary
+        if v.get('render', True):
+            try:
+                val = render_to_string(template, {
+                    'django_slack': 'django_slack/%s' % k,
+                }, context).strip().encode('utf8', 'ignore')
+            except Exception:
+                if fail_silently:
+                    return
+                raise
 
-    data = {
-        'text': '',
-        'token': app_settings.TOKEN,
-        'channel': app_settings.CHANNEL,
-        'icon_url': app_settings.ICON_URL,
-        'icon_emoji': app_settings.ICON_EMOJI,
-        'username': app_settings.USERNAME,
-        'attachments': attachments,
-        'endpoint_url': app_settings.ENDPOINT_URL,
-    }
+            if val:
+                data[k] = val
 
-    # Filter actually defined values
-    data = {k: v for k, v in data.items() if v}
+        # Check if paramater is required
+        if v['required']:
+            if data.get(k, None):
+                continue
 
-    # Render templates
-    for part in (
-        'token',
-        'channel',
-        'text',
-        'icon_url',
-        'icon_emoji',
-        'username',
-        'endpoint_url',
-    ):
-        try:
-            txt = render(part)
-        except Exception:
             if fail_silently:
                 return
-            raise
 
-        if txt:
-            data[part] = txt
-
-    # Check for required parameters
-    for x in (
-        'token',
-        'text',
-        'endpoint_url',
-    ):
-        if data.get(x, None):
-            continue
-
-        if fail_silently:
-            return
-
-        assert False, "Missing or empty required parameter: %s" % x
+            assert False, "Missing or empty required parameter: %s" % k
 
     # The endpoint URL is not part of the data payload but as we construct it
     # within `data` we must remove it.

commit cbb52d5061c99e68ec284e4e1717ab6c854d07c1

Correct custom endpoint_url (Closes: #32)
Broken in 49ff3c4fe261a8c6a139177c75900cf848e52300. Thanks @fero10101!

django_slack/api.py
@@ -44,7 +44,7 @@ def slack_message(template, context=None, attachments=None, fail_silently=app_se
         },
         'endpoint_url': {
             'default': app_settings.ENDPOINT_URL,
-            'render': False,
+            'render': True,
             'required': False,
         },
     }.items():