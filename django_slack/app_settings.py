8c6e9d7b719c41b67d80a62419385ea07a76c562
Chris Lamb <chris@chris-lamb.co.uk>
2016-02-26 12:17:57 +0000

    Rename to .get()

--- django_slack/app_settings.py
+++ django_slack/app_settings.py
@@ -2,26 +2,26 @@ from django.conf import settings
 
 
 class AppSettings(object):
-    def _get_setting(suffix, default):
+    def get(suffix, default):
         def setting(self):
             return getattr(settings, 'SLACK_%s' % suffix, default)
 
         return setting
 
-    TOKEN = property(_get_setting('TOKEN', None))
-    CHANNEL = property(_get_setting('CHANNEL', '#general'))
-    USERNAME = property(_get_setting('USERNAME', 'bot'))
-    ICON_URL = property(_get_setting('ICON_URL', None))
-    ICON_EMOJI = property(_get_setting('ICON_EMOJI', None))
+    TOKEN = property(get('TOKEN', None))
+    CHANNEL = property(get('CHANNEL', '#general'))
+    USERNAME = property(get('USERNAME', 'bot'))
+    ICON_URL = property(get('ICON_URL', None))
+    ICON_EMOJI = property(get('ICON_EMOJI', None))
 
     DEFAULT_ENDPOINT_URL = 'https://slack.com/api/chat.postMessage'
-    ENDPOINT_URL = property(_get_setting('ENDPOINT_URL', DEFAULT_ENDPOINT_URL))
+    ENDPOINT_URL = property(get('ENDPOINT_URL', DEFAULT_ENDPOINT_URL))
 
     _DEFAULT_BACKEND = 'django_slack.backends.DisabledBackend' if settings.DEBUG else \
         'django_slack.backends.UrllibBackend'
-    BACKEND = property(_get_setting('BACKEND', _DEFAULT_BACKEND))
-    BACKEND_FOR_QUEUE = property(_get_setting('BACKEND_FOR_QUEUE', _DEFAULT_BACKEND))
+    BACKEND = property(get('BACKEND', _DEFAULT_BACKEND))
+    BACKEND_FOR_QUEUE = property(get('BACKEND_FOR_QUEUE', _DEFAULT_BACKEND))
 
-    FAIL_SILENTLY = property(_get_setting('FAIL_SILENTLY', False))
+    FAIL_SILENTLY = property(get('FAIL_SILENTLY', False))
 
 app_settings = AppSettings()
