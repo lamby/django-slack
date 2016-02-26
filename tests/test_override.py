0a08bd29983b30e649d7ebfa8e9a7b516c5185f4
Chris Lamb <chris@chris-lamb.co.uk>
2016-02-26 12:24:42 +0000

    Reflow
    Signed-off-by: Chris Lamb <chris@chris-lamb.co.uk>

--- tests/test_override.py
+++ tests/test_override.py
@@ -9,24 +9,32 @@ from tests.backends import RaisingBackend
 
 class TestOverride(TestCase):
     def test_override(self):
-        """Ensure that accessing a setting in an override works."""
+        """
+        Ensure that accessing a setting in an override works.
+        """
         DISABLED_BACKEND = 'django_slack.backends.DisabledBackend'
         with override_settings(SLACK_BACKEND=DISABLED_BACKEND):
             self.assertEqual(settings.SLACK_BACKEND, DISABLED_BACKEND)
             self.assertEqual(app_settings.BACKEND, DISABLED_BACKEND)
 
     def test_backend_cache(self):
-        """Ensure that the backend is cached once it is called once."""
+        """
+        Ensure that the backend is cached once it is called once.
+        """
         self.assertEqual(id(get_backend()), id(get_backend()))
 
     def test_backend_override(self):
-        """Ensure the backend can be overridden."""
+        """
+        Ensure the backend can be overridden.
+        """
         with override_settings(SLACK_BACKEND='tests.backends.RaisingBackend'):
             self.assertRaises(RaisingBackend.RaisedException,
                               slack_message('test.slack', {'text': 'test'}))
 
     def test_fail_silently(self):
-        """Ensure fail silently can be overridden."""
+        """
+        Ensure fail silently can be overridden.
+        """
         # Note that this will fail if test_backend_override ever fails.
         with override_settings(SLACK_FAIL_SILENTLY=True,
                                SLACK_BACKEND='tests.backends.RaisingBackend'):
