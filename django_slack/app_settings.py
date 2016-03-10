58bd7a7e5e855724a69184240571689388da9f5f
Chris Lamb <chris@chris-lamb.co.uk>
2016-03-10 07:10:42 +0000

    Move these p

--- django_slack/app_settings.py
+++ django_slack/app_settings.py
@@ -7,17 +7,18 @@ def setting(suffix, default):
     return fn
 
 class AppSettings(object):
+    DEFAULT_ENDPOINT_URL = 'https://slack.com/api/chat.postMessage'
+    DEFAULT_BACKEND = 'django_slack.backends.DisabledBackend' if settings.DEBUG else \
+        'django_slack.backends.UrllibBackend'
+
     TOKEN = setting('TOKEN', None)
     CHANNEL = setting('CHANNEL', '#general')
     USERNAME = setting('USERNAME', 'bot')
     ICON_URL = setting('ICON_URL', None)
     ICON_EMOJI = setting('ICON_EMOJI', None)
 
-    DEFAULT_ENDPOINT_URL = 'https://slack.com/api/chat.postMessage'
     ENDPOINT_URL = setting('ENDPOINT_URL', DEFAULT_ENDPOINT_URL)
 
-    DEFAULT_BACKEND = 'django_slack.backends.DisabledBackend' if settings.DEBUG else \
-        'django_slack.backends.UrllibBackend'
     BACKEND = setting('BACKEND', DEFAULT_BACKEND)
     BACKEND_FOR_QUEUE = setting('BACKEND_FOR_QUEUE', DEFAULT_BACKEND)
 
