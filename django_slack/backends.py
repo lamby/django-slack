0c7c7cbd81d682a975958dbd3f86689e8b7fcd1d
yxy <yxy.870@gmail.com>
2016-05-21 21:10:18 +0800

    support rq and celery backends
    Signed-off-by: yxy <yxy.870@gmail.com>

--- django_slack/backends.py
+++ django_slack/backends.py
@@ -4,10 +4,8 @@ import logging
 from six.moves import urllib
 
 from django.http.request import QueryDict
-from django.utils.module_loading import import_string
 
 from .utils import Backend
-from .app_settings import app_settings
 
 logger = logging.getLogger(__name__)
 
@@ -52,35 +50,18 @@ class DisabledBackend(Backend):
         pass
 
 
-class _AsyncBackend(Backend):
-    def __init__(self):
-        self._send = self.init_sender()
+class CeleryBackend(Backend):
 
-        # Check we can import our specified backend up-front
-        import_string(app_settings.BACKEND_FOR_QUEUE)()
-
-    def send(self, *args, **kwargs):
-        # Send asynchronously via Celery
-        self._send.delay(*args, **kwargs)
-
-    def init_sender(self):
-        # Lazily import to avoid dependency
-        # subclass this must implement this method
-        raise NotImplementedError()
-
-
-class CeleryBackend(_AsyncBackend):
-
-    def init_sender(self):
+    def send(self, url, data, **kwargs):
         from .celery_task import celery_task
-        return celery_task
+        return celery_task.delay(url, data, **kwargs)
 
 
-class RQBackend(_AsyncBackend):
+class RQBackend(Backend):
 
-    def init_sender(self):
+    def send(self, url, data, **kwargs):
         from .tasks import get_rq_task
-        return get_rq_task()
+        return get_rq_task().delay(url, data, **kwargs)
 
 
 Urllib2Backend = UrllibBackend # For backwards-compatibility
--- tests/test_backends.py
+++ tests/test_backends.py
@@ -20,7 +20,6 @@ class TestCeleryBackends(TestCase):
         with override_settings(SLACK_BACKEND=BACKEND):
             backend = get_backend()
             self.assertIsInstance(backend, CeleryBackend)
-            self.assertTrue(backend._send is celery_task)
 
             # make sure we auto register the celery task
             self.assertEqual(tasks['django_slack.tasks._sender'], celery_task)
@@ -36,15 +35,17 @@ class TestRQBackends(TestCase):
 
     @mock.patch('django_slack.tasks.get_rq_task')
     @mock.patch('django_slack.backends.RQBackend.send')
-    def test_send(self, send, get_rq_worker):
+    def test_send(self, send, get_rq_task):
         BACKEND = 'django_slack.backends.RQBackend'
         with override_settings(
                 SLACK_RQ_QUEUE='default',
                 SLACK_BACKEND=BACKEND):
             backend = get_backend()
             self.assertIsInstance(backend, RQBackend)
-            self.assertTrue(get_rq_worker.called)
 
-            self.assertTrue(hasattr(backend._send, 'delay')) # is a rq job instance
+            worker = get_rq_task()
+            # check if we got the right job instance
+            self.assertTrue(hasattr(worker, 'delay'))
+
             backend.send()
             self.assertTrue(send.called)
