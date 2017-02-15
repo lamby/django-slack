c8c198e55ee946831bb73d9b1a856e7e7fc3ce31
Chris Lamb <chris@chris-lamb.co.uk>
2017-02-15 17:38:56 +1300

    Releasing version 5.8.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     name='django-slack',
 
     url="https://chris-lamb.co.uk/projects/django-slack",
-    version='5.7.1',
+    version='5.8.0',
     description="Provides easy-to-use integration between Django projects and "
         "the Slack group chat and IM tool.",
 
