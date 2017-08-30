lamby/django-slack

commit 0dfc8d10857d60a4bf95716551f95f7a1c77b25b

Clarify _which_ channel wasn't found
We have the data for this, it's just not being passed around at
the moment.

Note: this change is breaking for anyone who has subclassed the
exceptions this project defines or has created their own backend
and is using the '.validate' method.

django_slack/utils.py
@@ -10,14 +10,14 @@ class Backend(object):
     def send(self, url, data):
         raise NotImplementedError()
 
-    def validate(self, content_type, content):
+    def validate(self, content_type, content, original_data):
         if content_type.startswith('application/json'):
             result = json.loads(content)
 
             if not result['ok']:
                 klass = LABEL_TO_EXCEPTION.get(result['error'], SlackException)
 
-                raise klass(result['error'])
+                raise klass(result['error'], original_data)
 
         elif content != 'ok':
             raise SlackException(content)

django_slack/backends.py
@@ -24,7 +24,7 @@ class UrllibBackend(Backend):
 
         result = r.read().decode('utf-8')
 
-        self.validate(r.headers['content-type'], result)
+        self.validate(r.headers['content-type'], result, data)
 
 
 class RequestsBackend(Backend):
@@ -37,7 +37,7 @@ class RequestsBackend(Backend):
     def send(self, url, data, **kwargs):
         r = self.session.post(url, data=data, verify=False)
 
-        self.validate(r.headers['Content-Type'], r.text)
+        self.validate(r.headers['Content-Type'], r.text, data)
 
 
 class ConsoleBackend(Backend):

django_slack/exceptions.py
@@ -1,9 +1,19 @@
+import six
+
+
 class SlackException(ValueError):
-    pass
+    def __init__(self, message, sent_data):
+        super(SlackException, self).__init__(message)
+        self.sent_data = sent_data
 
 
+@six.python_2_unicode_compatible
 class ChannelNotFound(SlackException):
-    pass
+    def __str__(self):
+        return u"{}: channel '{}' could not be found".format(
+            self.__class__.__name__,
+            self.sent_data['channel'],
+        )
 
 
 class IsArchived(SlackException):

commit 6d897cec3c98384692ca996d94c1577a23b46381

Instantiation of SlackException requires argument 'message_data'
Broken in https://github.com/lamby/django-slack/commit/0dfc8d10857d60a4bf95716551f95f7a1c77b25b#diff-81a638ea9025632b2a95b6f35f70a5d7R5

django_slack/utils.py
@@ -20,7 +20,7 @@ class Backend(object):
                 raise klass(result['error'], message_data)
 
         elif content != 'ok':
-            raise SlackException(content)
+            raise SlackException(content, message_data)
 
 
 def get_backend(name=None):