cbab9cfcbb9d27063c7702961b0132c4633817e6
Jens Nistler <366967+lociii@users.noreply.github.com>
2020-11-23 18:18:22 +0100

    Make compatible with celery 5
    Use a shared task decorator as this is a lib, not an app.
    Please also consider properly adding the dependency to celery to the setup.py
    Fix for #97

--- django_slack/tasks.py
+++ django_slack/tasks.py
@@ -1,11 +1,11 @@
-from celery import task
+from celery import shared_task
 
 from django.utils.module_loading import import_string
 
 from .app_settings import app_settings
 
 
-@task
+@shared_task
 def send(*args, **kwargs):
     backend = import_string(app_settings.BACKEND_FOR_QUEUE)()
 
